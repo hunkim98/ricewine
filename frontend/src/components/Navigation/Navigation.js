ad01a8c7017bcc195ca467cc72ca43076543fb0a
ahanaha <tsotds101@gmail.com>
2021-05-30 01:17:59 +0900

    edited general details

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "../images/logo.png";
 import "./Navigation.css";
 import { NavLink } from "react-router-dom";
+import facebook from "../images/facebook-square-brands.svg";
+import instagram from "../images/instagram-square-brands.svg";
 
 function Navigation() {
   return (
     <div className="nav">
-      <img src={logo} />
+      <img id="logo" src={logo} />
       <br />
       <span className="menu_all">
         <span className="menu">
@@ -24,6 +26,16 @@ function Navigation() {
             Contact
           </NavLink>
         </span>
+
+        <div className="nav_icons">
+          <a href="https://www.facebook.com/cmakgeolli">
+            <img src={facebook} />{" "}
+          </a>
+          <a href="https://www.instagram.com/cmakgeolli">
+            <img src={instagram} />{" "}
+          </a>
+        </div>
+
       </span>
     </div>
   );
