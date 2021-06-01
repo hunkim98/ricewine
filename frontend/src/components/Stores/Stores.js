87e55b97b468a05335f5d80ef415649e15226b14
Kim Dong Hun <hunkim98@gmail.com>
2021-06-01 14:19:48 +0900

    Add short location information on right top store blank

--- frontend/src/components/Stores/Stores.js
+++ frontend/src/components/Stores/Stores.js
@@ -20,45 +20,48 @@ function Stores({ setClickMap, setAddress, setAddressName, setAddressInfo }) {
     <div className="contents">
       <div className="title">store</div>
       <div id="store_paragraph">
-          　매장마다 재고상황이 상이할 수 있으므로 방문하시고자 하는 가게에
-          재고가 있는지 미리 문의해보시기를 부탁드립니다!
+        　매장마다 재고상황이 상이할 수 있으므로 방문하시고자 하는 가게에 재고가
+        있는지 미리 문의해보시기를 부탁드립니다!
       </div>
       <hr size="2px" width="100%" color="black" />
       <div className="store_list">
-          {storeList.length == 0
-            ? null
-            : storeList.stores.map((item) => {
-                return (
-                  <div
-                    className="store"
-                    onClick={() => {
-                      setAddress([item.latitude, item.longditude]);
-                      setAddressName(item.name);
-                      setAddressInfo(item.description);
-                      setClickMap(true);
-                    }}
-                  >
-                    <div className="store_blank">
-                      <div className="store_name">{item.name}</div>
-                      <div className="store_ad">{item.location}</div>
-                    </div>
-                    <span className="lineup">
-                      {item.items.map((item_image) => {
-                        return (
-                          <div
-                            className="lineup_item"
-                            style={{
-                              backgroundImage: `url(${item_image.mainImage})`,
-                            }}
-                          >
-                            {item_image.itemName}
-                          </div>
-                        );
-                      })}
-                    </span>
+        {storeList.length == 0
+          ? null
+          : storeList.stores.map((item) => {
+              return (
+                <div
+                  className="store"
+                  onClick={() => {
+                    setAddress([item.latitude, item.longditude]);
+                    setAddressName(item.name);
+                    setAddressInfo(item.description);
+                    setClickMap(true);
+                  }}
+                >
+                  <div className="store_short_location">
+                    {item.location.substr(0, item.location.indexOf(" "))}
                   </div>
-                );
-          })}
+                  <div className="store_blank">
+                    <div className="store_name">{item.name}</div>
+                    <div className="store_ad">{item.location}</div>
+                  </div>
+                  <span className="lineup">
+                    {item.items.map((item_image) => {
+                      return (
+                        <div
+                          className="lineup_item"
+                          style={{
+                            backgroundImage: `url(${item_image.mainImage})`,
+                          }}
+                        >
+                          {item_image.itemName}
+                        </div>
+                      );
+                    })}
+                  </span>
+                </div>
+              );
+            })}
       </div>
     </div>
   );
