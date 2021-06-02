32e1bc1427cecbb053892d9f7b365819d17eb6ca
ahanaha <tsotds101@gmail.com>
2021-06-02 20:33:42 +0900

    edited contact css

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -8,18 +8,26 @@ function Contact() {
   return (
     <div className="contact">
       <div className="title" id="contact_title">Contact</div>
-      <div className="contact_icons">
-          <a href="https://www.facebook.com/cmakgeolli">
-            <img src={facebook} />{" "}
-          </a>
-          <a href="https://www.instagram.com/cmakgeolli">
-            <img src={instagram} />{" "}
-          </a>
+      <div className="contact_after_t">
+          <div className="contact_more">
+              <div className="contact_text">양조장 위치</div>
+              <div className="contact_text2">서울 강남구 논현로 18, 2층</div>
+              <br/>
+              <div className="contact_text">양조장 연락처</div>
+              <div className="contact_text2">010-xxxx-xxxx</div>
+              <br/>
+              <div className="contact_icons">
+                  <a href="https://www.facebook.com/cmakgeolli">
+                    <img src={facebook} />{" "}
+                  </a>
+                  <a href="https://www.instagram.com/cmakgeolli">
+                    <img src={instagram} />{" "}
+                  </a>
+              </div>
+          </div>
+          <ContactMap></ContactMap>
+          {/* ContactMap.js 안에 className="map" 존재 */}
       </div>
-      <div className="contact_text">양조장 위치</div>
-      <span className="contact_text2">서울 강남구 논현로 18, 2층</span>
-      <ContactMap></ContactMap>
-      {/* ContactMap.js 안에 className="map" 존재 */}
     </div>
   );
 }
