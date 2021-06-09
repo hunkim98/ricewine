706f67d4b5f373b9ac85e63db42d5f732c1a4921
Kim Dong Hun <hunkim98@gmail.com>
2021-06-09 09:41:08 +0900

    Delete unnecessary media setting from settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -142,11 +142,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join('static'), )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
