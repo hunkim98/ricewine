61b0abbfab214e691c07bd7e5e6f6eb0601385e7
Kim Dong Hun <hunkim98@gmail.com>
2021-06-09 22:40:13 +0900

    Fix allowed hosts to *

--- backend/settings.py
+++ backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 cloudinary.config(
     cloud_name="cmakgeoli",
